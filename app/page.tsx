4c1ce6263b0d1e7f71c1e43541f0a4391d9bcf0b
Shehan Samarasinghe <shehan.akhs@gmail.com>
2024-06-23 12:45:27 +1000

    Boilerplate with homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import Head from "next/head";
+import Hero from "./components/Hero";
 
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
