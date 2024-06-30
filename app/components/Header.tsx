ac579b10bbe0e3b5dcb1051546a2abe894b10f47
Shehan Samarasinghe <shehan.akhs@gmail.com>
2024-06-30 22:05:06 +1000

    changing the logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,25 +1,21 @@
 import { auth } from "@/auth";
 import { SignIn } from "./SignIn";
 import AuthButton from "./AuthButton";
+import Image from "next/image";
 
 async function Header() {
   const session = await auth();
   return (
-    <header className="text-gray-600 body-font">
+    <header className="text-gray-600 body-font bg-blue-100">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            stroke="currentColor"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-          </svg>
+          <Image
+            width={10}
+            height={10}
+            src="/images/logo.png"
+            alt="Logo"
+            className="w-10 h-10" //text-white p-2 bg-indigo-500 rounded-full
+          />
           <span className="ml-3 text-xl">Cosmic Blueprint</span>
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
