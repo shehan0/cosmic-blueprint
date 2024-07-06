01ebcddbce73beef767ff500dfa269900fd67701
Shehan Samarasinghe <shehan.akhs@gmail.com>
2024-07-07 09:58:55 +1000

    Chaning logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -18,10 +18,7 @@ async function Header() {
           />
           <span className="ml-3 text-xl">Cosmic Blueprint</span>
         </a>
-        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">About</a>
-          <a className="mr-5 hover:text-gray-900">Success Stories</a>
-        </nav>
+        <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center"></nav>
         <AuthButton />
       </div>
     </header>
